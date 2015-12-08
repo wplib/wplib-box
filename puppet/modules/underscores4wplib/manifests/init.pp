08844aa39ac7691f4744ef27f9de66d8d59c7a38
Daryl Lozupone <daryl@clubdeuce.ninja>
2015-12-08 11:56:49 -0500

    Update to WP-Redis 0.2.2 Simplify Redis configuration Resolves #2

--- puppet/modules/underscores4wplib/manifests/init.pp
+++ puppet/modules/underscores4wplib/manifests/init.pp
@@ -1,8 +1,10 @@
 class underscores4wplib {
   exec { 'composer install':
-    cwd          => '/var/www/underscores4wplib',
+    cwd         => '/var/www/underscores4wplib',
     environment => ['HOME=/home/vagrant'],
-    path         => ['/usr/local/bin/', '/usr/bin'],
+    path        => ['/usr/local/bin/', '/usr/bin'],
+    user        => 'vagrant',
+    group       => 'vagrant'
   }
   file { '/etc/nginx/sites-available/underscores.conf':
     ensure   => file,
@@ -26,5 +28,10 @@ class underscores4wplib {
     grant          => ['ALL'],
     sql            => '/vagrant/puppet/modules/underscores4wplib/files/import.sql',
     import_timeout => 900
+  }
+  file { '/var/www/underscores4wplib/www/content/object-cache.php':
+    ensure    => link,
+    target    => '/var/www/underscores4wplib/www/content/plugins/wp-redis/object-cache.php',
+    subscribe => Exec['composer install'],
   }
 }
