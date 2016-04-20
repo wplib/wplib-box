aa53357b1cdecffb3fa5685bf3925e720ac26541
Mike Schinkel <mikeschinkel@gmail.com>
2016-04-20 04:07:01 -0400

    Expanded on provision.sh adding an error message when "composer install" was not yet run AND the -f option to `ln` command to force link creation even if it already exists.

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,3 +1,26 @@
 #!/usr/bin/env bash
-sudo ln -s /var/www/content/mu-plugins/wp-redis/object-cache.php /var/www/content/object-cache.php
+#
+# WPLib Box provision script
+#
+
+#
+# Import default database from /sql/default.sql
+#
+
 mysql -u root wordpress < /vagrant/sql/default.sql
+
+#
+# Symlink the Object Cache Drop-in for Redis, if not already symlinked
+#
+content_dir="/var/www/content"
+cache_file="${content_dir}/mu-plugins/wp-redis/object-cache.php"
+cache_link="${content_dir}/object-cache.php"
+if [ ! -f "${cache_file}" ]; then
+    echo "ERROR: Composer not run before Vagrant. Please run these two (2) commands:"
+    echo " "
+    echo "      composer install"
+    echo "      vagrant reload --provision"
+    echo " "
+else
+    sudo ln -sf "${cache_file}" "${cache_link}"
+fi
