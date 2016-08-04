93156b03ecc140984b7ff42ad16b735197b24912
Mike Schinkel <mikeschinkel@gmail.com>
2016-08-04 05:17:24 -0400

    Pipe output to null for clone of Box CLI

--- scripts/guest/provision.sh
+++ scripts/guest/provision.sh
@@ -6,7 +6,7 @@
 #
 # Install the Box CLI for use inside the Guest, e.g. after `vagrant ssh`
 #
-git clone https://github.com/wplib/box-cli.git /vagrant/scripts/guest/cli
+git clone https://github.com/wplib/box-cli.git /vagrant/scripts/guest/cli  2>/dev/null
 cd /vagrant/scripts/guest/cli
 sh ./install-box-cli.sh
 
