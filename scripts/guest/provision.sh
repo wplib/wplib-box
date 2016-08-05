2da67b810ee6698e0e43a9a644b216c904d010bd
Mike Schinkel <mikeschinkel@gmail.com>
2016-08-05 03:03:25 -0400

    Added status messages to provision script.

--- scripts/guest/provision.sh
+++ scripts/guest/provision.sh
@@ -6,6 +6,7 @@
 #
 # Install the Box CLI for use inside the Guest, e.g. after `vagrant ssh`
 #
+echo "Installing Box CLI..."
 git clone https://github.com/wplib/box-cli.git /vagrant/scripts/guest/cli  2>/dev/null
 cd /vagrant/scripts/guest/cli
 ./install-box-cli --quiet
@@ -13,11 +14,13 @@ cd /vagrant/scripts/guest/cli
 #
 #  Disassociate the cloned Git repo and initialize a new repo.
 #
+echo "Disassociating WPLib Box Git repo..."
 box disassociate-git-repo --quiet
 
 #
 #  Enable Object Caching
 #
+echo "Enable Redis-based Object Caching..."
 box enable-object-caching --force --quiet
 
 echo
