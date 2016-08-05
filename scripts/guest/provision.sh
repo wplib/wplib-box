ff84c02f965f23a906f334044f596fbe2bff7e7c
Mike Schinkel <mikeschinkel@gmail.com>
2016-08-05 07:10:51 -0400

    tiny change

--- scripts/guest/provision.sh
+++ scripts/guest/provision.sh
@@ -20,7 +20,7 @@ box disassociate-git-repo --quiet
 #
 #  Enable Object Caching
 #
-echo "Enabling Redis-based Object Caching... (IF WordPress is installed)"
+echo "Enabling Redis-based Object Caching..."
 box enable-object-caching --force --quiet
 
 #
