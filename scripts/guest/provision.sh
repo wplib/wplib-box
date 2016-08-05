da9f5ce3520a21da0478a7eca3f3259131929448
Mike Schinkel <mikeschinkel@gmail.com>
2016-08-05 06:10:16 -0400

    Tiny changes in output

--- scripts/guest/provision.sh
+++ scripts/guest/provision.sh
@@ -6,7 +6,7 @@
 #
 # Install the Box CLI for use inside the Guest, e.g. after `vagrant ssh`
 #
-echo "Installing Box CLI..."
+echo "Installing Box CLI... (try: 'box help')"
 git clone https://github.com/wplib/box-cli.git /vagrant/scripts/guest/cli  2>/dev/null
 cd /vagrant/scripts/guest/cli
 ./install-box-cli --quiet
@@ -20,7 +20,7 @@ box disassociate-git-repo --quiet
 #
 #  Enable Object Caching
 #
-echo "Enable Redis-based Object Caching..."
+echo "Enabling Redis-based Object Caching..."
 box enable-object-caching --force --quiet
 
 
@@ -30,6 +30,5 @@ box enable-object-caching --force --quiet
 #echo "Enable File Watchers..."
 #box enable-file-watchers --quiet
 
-echo
 echo "Congratulations! WPLib Box is now installed!"
 
