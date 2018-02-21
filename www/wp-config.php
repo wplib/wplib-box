01fefd1fb83d64ea8d2e3ed69d75932de79794e0
Mike Schinkel <mike@newclarity.net>
2018-02-20 20:32:07 -0500

    Added both WPLIB_BOX_HOST and WPLIB_BOX_LOCAL_CONFIG constants, and pulling database credentials from $_SERVER.  Also, discovering WPLIB_BOX_DIRECTORY_LAYOUT based on /wp-include/ and not on /wp-content/.

--- www/wp-config.php
+++ www/wp-config.php
@@ -4,34 +4,42 @@ if ( defined( 'WP_CLI' ) && WP_CLI ) {
 	$_SERVER[ 'HTTP_HOST' ] = 'wplib.box';
 }
 
+if ( ! isset( $_SERVER[ 'HTTP_HOST' ] ) ) {
+	trigger_error( '$_SERVER[ \'HTTP_HOST\' ] not set (server may be misconfigured)' );
+	exit;
+}
+
+define( 'WPLIB_BOX_HOST', preg_match( '#^www\.(.+)$#', $_SERVER[ 'HTTP_HOST' ] )
+	? preg_replace( '#^www\.(.+)$#', '$1', $_SERVER[ 'HTTP_HOST' ] )
+	: $_SERVER[ 'HTTP_HOST' ]
+);
+
+define( 'WPLIB_BOX_LOCAL_CONFIG', '/wp-config-' . WPLIB_BOX_HOST . '.php' );
+
+
 /**
  * Search for a wp-config-{HTTP_HOST}.php in current
  * and parent directories for config overrides
- */ 
-if ( file_exists( __DIR__ . "/wp-config-{$_SERVER[ 'HTTP_HOST' ]}.php" ) ) {
-	require( __DIR__ . "/wp-config-{$_SERVER[ 'HTTP_HOST' ]}.php" );
-} else if ( file_exists( dirname( __DIR__ ) . "/wp-config-{$_SERVER[ 'HTTP_HOST' ]}.php" ) ) {
-	require( dirname( __DIR__ ) . "/wp-config-{$_SERVER[ 'HTTP_HOST' ]}.php" );
+ */
+if ( file_exists( __DIR__ . WPLIB_BOX_LOCAL_CONFIG ) ) {
+	require( __DIR__ . WPLIB_BOX_LOCAL_CONFIG );
+} else if ( file_exists( dirname( __DIR__ ) . WPLIB_BOX_LOCAL_CONFIG ) ) {
+	require( dirname( __DIR__ ) . WPLIB_BOX_LOCAL_CONFIG );
 }
 
 if ( ! defined( 'WPLIB_BOX_DIRECTORY_LAYOUT' ) ) {
-	if ( is_dir( __DIR__ . '/wp-content' ) ) {
+	if ( is_dir( __DIR__ . '/wp-include' ) ) {
 		define( 'WPLIB_BOX_DIRECTORY_LAYOUT', 'standard' );
-	} else if ( is_dir( __DIR__ . '/wp/wp-content' ) ) {
+	} else if ( is_dir( __DIR__ . '/wp/wp-include' ) ) {
 		define( 'WPLIB_BOX_DIRECTORY_LAYOUT', 'skeleton' );
 	} else {
-		trigger_error( 'WordPress content directory not found (expected at ' . __DIR__ . '/wp-content/)' );
+		trigger_error( 'WordPress includes directory not found (expected at ' . __DIR__ . '/wp-include/)' );
 		exit;
 	}
 }
 
 if ( ! defined( 'WPLIB_BOX_URL_SCHEME' ) ) {
 	define( 'WPLIB_BOX_URL_SCHEME', 'https' );
-}
-
-if ( ! isset( $_SERVER[ 'HTTP_HOST' ] ) ) {
-	trigger_error( '$_SERVER[ \'HTTP_HOST\' ] not set (server may be misconfigured)' );
-	exit;
 }
 
 if ( ! defined( 'SITE_DOMAIN' ) ) {
@@ -50,15 +58,15 @@ if ( 'standard' === WPLIB_BOX_DIRECTORY_LAYOUT ) {
 }
 
 if ( ! defined( 'DB_NAME' ) ) {
-	define( 'DB_NAME', 'wordpress' );
+	define( 'DB_NAME', $_SERVER[ 'DB_NAME' ] );
 }
 
 if ( ! defined( 'DB_USER' ) ) {
-	define( 'DB_USER', 'wordpress' );
+	define( 'DB_USER',  $_SERVER[ 'DB_USER' ] );
 }
 
 if ( ! defined( 'DB_PASSWORD' ) ) {
-	define( 'DB_PASSWORD', 'wordpress' );
+	define( 'DB_PASSWORD',  $_SERVER[ 'DB_PASSWORD' ] );
 }
 
 if ( ! defined( 'DB_HOST' ) ) {
@@ -92,8 +100,8 @@ if ( ! isset( $table_prefix ) ) {
 /**
  * https://api.wordpress.org/secret-key/1.1/salt/
  */
-if ( file_exists( __DIR__ . "/salt-{$_SERVER[ 'HTTP_HOST' ]}.php" ) ) {
-	require( __DIR__ . "/salt-{$_SERVER[ 'HTTP_HOST' ]}.php" );
+if ( file_exists( __DIR__ . '/salt-' . WPLIB_BOX_HOST . '.php' ) ) {
+	require( __DIR__ . '/salt-' . WPLIB_BOX_HOST . '.php' );
 } else {
 	define('AUTH_KEY',         'Insecure' );
 	define('SECURE_AUTH_KEY',  'Insecure' );
